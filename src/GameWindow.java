3bdc43a53db1f72567ca8b5422b749971c96eca9
honguyen <honguyen2996@gmail.com>
2017-02-21 02:27:55 -0800

    :)

---
+++ src/GameWindow.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+/**
+ * Created by HongUyen on 21-Feb-17.
+ */
+public class GameWindow extends Frame{
+    
+}
---
+++ src/Program.java
@@ -0,0 +1,8 @@
+/**
+ * Created by HongUyen on 21-Feb-17.
+ */
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
